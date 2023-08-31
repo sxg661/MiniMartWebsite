b07dbf6a162fe494d2843b0ca4b0d39193762270
sxg661 <sophie.guile@yahoo.co.uk>
2023-08-31 23:42:17 +0100

    remove import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './css/App.css';
 import MainPage from './react/pages/MainPage';
-import callAPI from './api_calls/testAPI';
 
 function App() {
 
-  callAPI(console.log);
-  
   return (
     <div className="App">
       <MainPage/>
