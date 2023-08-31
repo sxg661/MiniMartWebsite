8382cd38401fadd888057f864c5e6b780090d926
sxg661 <sophie.guile@yahoo.co.uk>
2023-08-31 23:18:50 +0100

    make this repo just client not client and api - probably wont need api

--- api/routes/testAPI.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-    console.log("Api works test endpoint reached");
-    res.send('APIWORKS');
-});
-
-module.exports = router;
