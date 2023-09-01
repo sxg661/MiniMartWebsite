cf3d94961f95270fa64c696d743c98fd3b8a8c15
sxg661 <sophie.guile@yahoo.co.uk>
2023-09-02 00:04:01 +0100

    remove console log

--- src/api/apiTest.js
+++ src/api/apiTest.js
@@ -2,7 +2,6 @@ import GetApiUrl from './ApiHelper';
 
 export default async function callAPI(callback) {
     var url = GetApiUrl() + "/testAPI";
-    console.log(url);
     const res = await fetch(url);
     const restext = await res.text()
     callback(restext);
