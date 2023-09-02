e864a1fc8551bd196842d9401ac1d6ed530be002
sxg661 <sophie.guile@yahoo.co.uk>
2023-09-02 17:10:39 +0100

    add proper routing

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
 import './css/App.css';
 import MainPage from './react/pages/MainPage';
-import ErrorPage from './react/pages/ErrorPage';
 import SinglePostPage from './react/pages/SinglePostPage';
-import React from 'react';
 import PostContext from './contexts/PostContext';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
 
-  function PageToShow() {
-    const pagePath = window.location.pathname.toLowerCase();
-
-    if(pagePath === "/") {
-      return <MainPage/>
-    }
-    
-    if(pagePath.includes("/post")){
-      var postId = pagePath.replace("/post", "").replace("/", "");
-      return(
-        <PostContext.Provider value = {{id: postId}}>
-          <SinglePostPage/>
-        </PostContext.Provider>
-      )
-    }
-
-    return <ErrorPage/>
+  function PostComponent() {
+    return (
+      <PostContext.Provider value = {{id: "temp"}}>
+        <SinglePostPage/>
+      </PostContext.Provider>)
   }
 
   return (
     <div className="App">
-      {PageToShow()}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage/>} />
+          <Route path="/post" element={PostComponent()} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
