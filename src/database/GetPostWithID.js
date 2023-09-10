ad3e10fbb6c903d279ded6862c8e921f3c5cd4dc
sxg661 <sophie.guile@yahoo.co.uk>
2023-09-11 00:30:24 +0100

    add single post page for showing a post with an id from the database

--- src/database/GetPostWithID.js
+++ src/database/GetPostWithID.js
@@ -1,10 +1,10 @@
 import firestoreDb from "./firebase"
 import { collection, doc, getDoc } from "firebase/firestore";
 
-export default async function GetPostWithId(id) {
+export default async function GetPostWithId(id, callback) {
     var collectionRef = collection(firestoreDb, "posts");
     var docRef = doc(collectionRef, id);
     var post = await getDoc(docRef);
     console.log(post.data());
-    return post.data();
+    callback(post.data());
 };
--- src/react/pages/SinglePostPage.js
+++ src/react/pages/SinglePostPage.js
@@ -1,14 +1,38 @@
 import { useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import parse from 'html-react-parser';
 import GetPostWithId from "../../database/GetPostWithID";
 
 function SinglePostPage() {
     const [searchParams] = useSearchParams();
 
-    GetPostWithId(searchParams.get('id'));
+    const [postData, setPostData] = useState({});
+    const [id] = useState(searchParams.get('id'));
+    const [postHtml, setPostHtml] = useState("");
+    const [postDateTime, setPostDateTime] = useState("");
+
+    const convertTimestampToString = (timestamp) => {
+        return timestamp.toDate().toUTCString();
+    }
+
+    useEffect(() => {
+        GetPostWithId(id, setPostData);
+    }, [id]);
+
+    useEffect(() => {
+        setPostHtml(postData && postData.html ? postData.html : "");
+        if(postData && postData.time){
+            console.log(postData.time);
+        }
+        setPostDateTime(postData && postData.time ? convertTimestampToString(postData.time) : "");
+    }, [postData, setPostData]);
 
     return (
-        <div className="test">
-            A Post with id {searchParams.get('id')}
+        <div className="single-post">
+            <div className="post-container">
+                <div className="post-date">{postDateTime}</div>
+                {parse(postHtml)}
+            </div>
         </div>
     );
     }
