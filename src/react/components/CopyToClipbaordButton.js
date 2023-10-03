7b59e9b4b7fd8ec72c38052fa977a93c561ae6f1
sxg661 <sophie.guile@yahoo.co.uk>
2023-10-03 23:45:41 +0100

    improve share button

--- src/react/components/CopyToClipbaordButton.js
+++ src/react/components/CopyToClipbaordButton.js
@@ -1,13 +1,27 @@
-import ClipboardIcon from "./ClipboardIcon";
+import { useState } from "react";
 import CopyToClipboard from "react-copy-to-clipboard";
+import { format } from "react-string-format";
+import ClipboardIcon from "./ClipboardIcon";
 
 export default function CopyToClipboardButton(props) {
+    const [showCopyNotice, setShowCopyNotice] = useState(false);
+
+    const getHiddenClass = () => showCopyNotice ? "" : "copy-to-clipboard-button-copied-notice-hidden";
+
+    function displayCopyNotice(){
+        setShowCopyNotice(true);
+
+        setTimeout(() => setShowCopyNotice(false), 2000);
+    }
+
     return(
-        <div className="sharable-link-button">
+        <div className="copy-to-clipboard-button">
             <CopyToClipboard text={props.text}
-                onCopy={() => {}}>
+                onCopy={displayCopyNotice}>
                 <span>{ClipboardIcon()} {props.displayText}</span>
-            </CopyToClipboard> 
+            </CopyToClipboard>
+
+            <div class= {format("copy-to-clipboard-button-copied-notice {0}", getHiddenClass())}>Copied</div> 
         </div>
     );
 }
